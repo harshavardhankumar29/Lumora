4ba66d17a32f10834ed1a79612a05fcccdb5a56e
harshavardhankumar29 <hharsha2904@gmail.com>
2026-03-16 23:24:30 +0530

    Started working on user service

---
+++ services/user/src/controllers/user.ts
@@ -0,0 +1,8 @@
+import { AuthenticatedRequest } from "../middleware/auth.js";
+import { TryCatch } from "../utils/TryCatch.js";
+
+export const myProfile = TryCatch(async (req: AuthenticatedRequest, res, next) => {
+    const user = req.user;
+
+    res.json(user);
+});
